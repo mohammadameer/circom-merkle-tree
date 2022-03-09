f4ee291998f5a37b93b7e4a794bfda6b5a954960
mohammad ameer <mohammadameerabdallah@gmail.com>
2022-03-09 07:57:56 +0200

    add coments for the execute bash

--- execute.sh
+++ execute.sh
@@ -10,28 +10,43 @@ rm verification_key.json
 rm witness.wtns
 rm parameters.txt
 
+# compile our circuit and generate
+# the r1cs constraints
+# the wasm code to generate the witness
+# symbols file that can be used for debugging
 circom circuit.circom --r1cs --wasm --sym 
 
+# computing the witness
 cd circuit_js
 node generate_witness.js circuit.wasm ../input.json witness.wtns
 
 cp witness.wtns ../witness.wtns
 cd ..
 
-snarkjs powersoftau new bn128 16 pot16_0000.ptau -v
+# now we  should creat a trusted setup to generate the proof
 
-snarkjs powersoftau contribute pot16_0000.ptau pot16_0001.ptau --name="First contribution" -v
+# create new power of tau
+snarkjs powersoftau new bn128 13 pot13_0000.ptau -v
 
-snarkjs powersoftau prepare phase2 pot16_0001.ptau pot16_final.ptau -v
+# contribute to the ceremony
+snarkjs powersoftau contribute pot13_0000.ptau pot13_0001.ptau --name="First contribution" -v -e="random text"
 
-snarkjs groth16 setup circuit.r1cs pot16_final.ptau circuit_0000.zkey
+# prepare for phase2
+snarkjs powersoftau prepare phase2 pot13_0001.ptau pot13_final.ptau -v
 
-snarkjs zkey contribute circuit_0000.zkey circuit_0001.zkey --name="1st Contributor Name" -v
+# generate zkey file that contain the proof and verfication key
+snarkjs groth16 setup circuit.r1cs pot13_final.ptau circuit_0000.zkey
 
+# contribute to phsate2 ceremony
+snarkjs zkey contribute circuit_0000.zkey circuit_0001.zkey --name="1st Contributor Name" -v -e="random text"
+
+# export verification key
 snarkjs zkey export verificationkey circuit_0001.zkey verification_key.json
 
+# now the witness is computed and trusted setup is ready we generate the proof 
 snarkjs groth16 prove circuit_0001.zkey witness.wtns proof.json public.json
 
+# now we can verify the proof
 snarkjs groth16 verify verification_key.json public.json proof.json
 
 # create solidity file
